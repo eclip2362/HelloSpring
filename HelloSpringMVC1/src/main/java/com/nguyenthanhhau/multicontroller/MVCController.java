7d534fbbbfbff6ea8de8a5c31350a6da099dee84
Akali <Akali@My-PC>
2020-10-03 18:51:10 +0700

    1

---
+++ HelloSpringMVC1/src/main/java/com/nguyenthanhhau/multicontroller/MVCController.java
@@ -0,0 +1,21 @@
+package com.nguyenthanhhau.multicontroller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MVCController {
+  @RequestMapping("/mvc")
+  public ModelAndView showMVC() {
+		  ModelAndView mv=new ModelAndView("view_mvc");
+		  return mv;
+  }	  
+  @RequestMapping("/boot")
+   public ModelAndView showMVC1() {
+	  ModelAndView mv=new ModelAndView("view_boot");
+	  return mv;
+  }
+	  
+}
